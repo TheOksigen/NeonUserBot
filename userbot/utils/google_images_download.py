44bd543bb251f871a4771c9a4f1816dafdc428c5
Nusret <89344065+nusrte@users.noreply.github.com>
2021-10-09 17:37:24 +0400

    `Code written 2 times`

--- userbot/utils/google_images_download.py
+++ userbot/utils/google_images_download.py
@@ -643,8 +643,6 @@ class googleimagesdownload:
                 output_file.write(data)
         except OSError as e:
             raise e
-        except OSError as e:
-            raise e
         print(
             "completed ====> " + image_name.encode("raw_unicode_escape").decode("utf-8")
         )
@@ -991,11 +989,6 @@ class googleimagesdownload:
                     download_status = "fail"
                     download_message = (
                         "OSError on an image...trying next one..." + " Error: " + str(e)
-                    )
-                except OSError as e:
-                    download_status = "fail"
-                    download_message = (
-                        "IOError on an image...trying next one..." + " Error: " + str(e)
                     )
 
                 download_status = "success"
