24fc4a5229d7af1df5e6a3890ca21afbec3c544f
Nusret <89344065+nusrte@users.noreply.github.com>
2021-10-13 22:46:23 +0400

    Update help.py

--- helper/help.py
+++ helper/help.py
@@ -3,16 +3,20 @@
 # Nusrte
 # 
 
+
 import os
+import shlex
 import random
 import asyncio
 import pybase64
 import PIL.ImageOps
 from PIL import Image
+from userbot import LOGS
 from os.path import basename
 from random import choice, randint
 from typing import Optional, Tuple
 
+# --------------------------------- #
 
 async def convert_toimage(image):
     img = Image.open(image)
